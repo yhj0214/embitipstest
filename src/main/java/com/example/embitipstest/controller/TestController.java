223f585a282cc45658f21d5e8f61e3d054426f32
yuhojae <ho_0214@naver.com>
2025-02-03 23:38:43 +0900

    test controller

---
+++ src/main/java/com/example/embitipstest/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.embitipstest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("test");
+        return "test";
+    }
+}
