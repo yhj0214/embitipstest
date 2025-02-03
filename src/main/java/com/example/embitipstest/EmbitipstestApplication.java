61241202c84de3239ffc75117c320e7e94782bed
yuhojae <ho_0214@naver.com>
2025-02-03 23:21:32 +0900

    Add deploy.tml for CI/CD

---
+++ src/main/java/com/example/embitipstest/EmbitipstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.embitipstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbitipstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbitipstestApplication.class, args);
+    }
+
+}
